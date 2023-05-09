499c1b00376c083d7eefaf46c864047bdbe5901e
Spencer Longhurst <spencersean12@gmail.com>
2023-05-08 20:17:31 -0600

    Initial Commit

---
+++ CleanAer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanAer
+//
+//  Created by Spencer Longhurst on 5/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
